7f340473d837a9c920e7af169421e2d8b1a15ee6
Jahnavi Kaasu <1kasujah@hdsb.ca>
2025-06-14 11:41:00 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-
+/*
 function App() {
   return (
     <div className="App">
@@ -18,6 +18,14 @@ function App() {
           Learn React
         </a>
       </header>
+    </div>
+  );
+}
+*/
+function App() {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
     </div>
   );
 }
